3a7af8efabb4c75189ed145467d84d7684c655da
sriram8488 <srirampadhy8488@mail.com>
2019-01-10 12:20:22 +0530

    added newjava.bat

---
+++ newjava.bat
@@ -0,0 +1,5 @@
+set projectLocation=D:\Testing\Selenium\Ram\Way_SMS
+cd %projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+java org.testng.TestNG %projectLocation%\testng.xml
+pause
