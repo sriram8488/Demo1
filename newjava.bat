ab09e2b483a591a72401c933468c6a66d17c459d
sriram8488 <srirampadhy8488@mail.com>
2019-01-10 12:27:43 +0530

    deleted newjava.bat

--- newjava.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=D:\Testing\Selenium\Ram\Way_SMS
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
